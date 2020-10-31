e86e754e2d52379ad8e8140fb205f0d68250a766
Jon Snyder <snyder.jon@gmail.com>
2020-10-30 17:45:01 -0700

    update generated locales

--- resources/internal/locales/ms_ID.go
+++ resources/internal/locales/ms_ID.go
@@ -4,8 +4,6 @@ package locales
 import (
 	"github.com/razor-1/cldr"
 	"github.com/razor-1/cldr/resources/currency"
-	"github.com/razor-1/cldr/resources/language"
-	"github.com/razor-1/cldr/resources/territory"
 )
 
 func Get_ms_ID() *cldr.Locale {
